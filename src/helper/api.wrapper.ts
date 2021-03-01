5c27f71e16e4965f1cb21daebccbf0dde02876e3
daniel mwangila <margalight@outlook.com>
2021-03-01 22:19:25 +0200

    include .env

--- src/helper/api.wrapper.ts
+++ src/helper/api.wrapper.ts
@@ -60,7 +60,10 @@ class APIWrapper {
       res.setHeader('Access-Control-Allow-Origin', '*');
       res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
       res.setHeader('Access-Control-Max-Age', 2592000);
-      res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+      res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json',
+      );
       res.setHeader('Access-Control-Allow-Credentials', 'true');
 
       if (req.method === 'OPTIONS') {
