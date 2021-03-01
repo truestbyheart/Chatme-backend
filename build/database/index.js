b5162e50c57422ca1fc54e5b7730f3ee268b6026
daniel mwangila <margalight@outlook.com>
2021-03-01 21:39:56 +0200

    remove build folder from git cache

--- build/database/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var pg_1 = require("pg");
-var dotenv_1 = require("dotenv");
-dotenv_1.config();
-var DATABASE_URL = process.env.DATABASE_URL;
-var client = new pg_1.Client({ connectionString: DATABASE_URL });
-client.connect();
-exports.default = client;
